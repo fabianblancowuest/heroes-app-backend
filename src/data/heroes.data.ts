5bb95749ed29b1d29c21a38f9811a8a795948f2e
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-03-09 15:19:25 -0300

    feat: add new antihero characters

--- src/data/heroes.data.ts
+++ src/data/heroes.data.ts
@@ -2,9 +2,11 @@ import { Hero } from 'src/heroes/entities/hero.entity';
 import { charactersOne } from './characters_one.data';
 import { charactersTwo } from './characters_two.data';
 import { moreVillains } from './more_villains.data';
+import { moreAntiheroes } from './more_antiheroes.data';
 
 export const heroesData: Hero[] = [
   ...charactersOne,
   ...charactersTwo,
   ...moreVillains,
+  ...moreAntiheroes,
 ];
